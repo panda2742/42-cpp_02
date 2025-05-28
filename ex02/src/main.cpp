2ef5d297166a3d005fd223ffed696967a25c0aff
elouannh <elouannhosta27@gmail.com>
2025-05-28 13:27:09 +0200

    Pushing tests and implementations for ex02

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -2,9 +2,36 @@
 #include <iostream>
 
 int main(void) {
-	Fixed		a(5);
-	Fixed		b(0);
+	Fixed	a(5), b, c, d, e, f, g, h, i;
 
-	a / b;
+	std::cout << "\033[1;35ma is " << a << "\033[0m" << std::endl;
+
+	b = a + a;
+	std::cout << "\033[1;35mb is " << b << "\033[0m" << std::endl;
+
+	c = a - a;
+	std::cout << "\033[1;35mc is " << c << "\033[0m" << std::endl;
+
+	d = a * a;
+	std::cout << "\033[1;35md is " << d << "\033[0m" << std::endl;
+
+	e = a / a;
+	std::cout << "\033[1;35me is " << e << "\033[0m" << std::endl;
+
+	f = a++;
+	std::cout << "\033[1;35mf is " << f << "\033[0m" << std::endl;
+	std::cout << "\033[1;35ma is " << a << "\033[0m" << std::endl;
+
+	g = ++a;
+	std::cout << "\033[1;35mg is " << g << "\033[0m" << std::endl;
+	std::cout << "\033[1;35ma is " << a << "\033[0m" << std::endl;
+
+	h = a--;
+	std::cout << "\033[1;35mh is " << h << "\033[0m" << std::endl;
+	std::cout << "\033[1;35ma is " << a << "\033[0m" << std::endl;
+
+	i = --a;
+	std::cout << "\033[1;35mi is " << i << "\033[0m" << std::endl;
+	std::cout << "\033[1;35ma is " << a << "\033[0m" << std::endl;
 	return 0;
 }
